d6b53ac129eef9fb79251d45fcd41a203379a719
Norhan Ibrahim <norhanibrahim289@gmail.com>
2024-03-31 19:25:20 +0200

    SettingPage

--- lib/screens/setting/setting_view.dart
+++ lib/screens/setting/setting_view.dart
@@ -5,11 +5,16 @@ import 'package:care_app_two/helper/functions/custom_chat_bot_appbar.dart';
 import 'package:care_app_two/helper/styles.dart';
 import 'package:care_app_two/screens/widgets/custom_line_setting.dart';
 import 'package:care_app_two/screens/widgets/custom_settingrow.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
+
 
 class SettingView extends StatelessWidget {
   const SettingView({super.key});
+
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +50,9 @@ class SettingView extends StatelessWidget {
                       children: [
                         SetRow(
                           text: 'General Notification',
-                          icon: (Icons.toggle_on),
+                          icon: Icons.toggle_on,
+                         
+                          //icon: (),
                         ),
                         LineSetting(),
                         SetRow(
@@ -125,3 +132,145 @@ class SettingView extends StatelessWidget {
         ));
   }
 }
+
+
+/*class SettingView extends StatefulWidget {
+  const SettingView({super.key});
+
+  @override
+  State<SettingView> createState() => _SettingViewState();
+}
+
+class _SettingViewState extends State<SettingView> {
+  bool val1 = true;
+  onChangedFunction1(bool newvalue1){
+    setState((){
+      val1 = newvalue1;
+    });
+  }
+   bool val2 = false;
+  onChangedFunction2(bool newvalue2){
+    setState((){
+      val1 = newvalue2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CustomCareAppbar(context, 'Setting'),
+        extendBodyBehindAppBar: true,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(kBackgroundStart), fit: BoxFit.fill),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ListView(
+              children: [
+                SizedBox(height: 56.h),
+                Text(
+                  'Notification',
+                  style: Styles.Style166,
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                Container(
+                  height: 280.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color(0xfff6f6f6),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 20),
+                    child: Column(
+                      children: [
+                        SetRow(
+                          
+                          CustomSwitch( val1, onChangedFunction1)  ,
+                          text: 'General Notification', 
+                         // LiteRollingSwitch(onChanged: (bool ) { on; },),
+                          //icon: (),
+                        ),
+                        LineSetting(),
+                        SetRow(
+                          text: 'Sound',
+                        ),
+                        LineSetting(),
+                        SetRow(
+                          text: 'Vibrate ',
+                          //icon: (Icons.toggle_off),
+                        ),
+                        LineSetting(),
+                        SetRow(
+                          text: ' App Updates',
+                          icon: (Icons.toggle_on),
+                        ),
+                        LineSetting(),
+                        SetRow(
+                          text: ' New Service Available',
+                          icon: (Icons.toggle_on),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 47.h,
+                ),
+                Container(
+                    height: 98.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xfff6f6f6),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, top: 11),
+                      child: Column(children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(Icons.privacy_tip_outlined),
+                              SizedBox(
+                                width: 7.w,
+                              ),
+                              Text(
+                                'Privacy Policy',
+                                style: Styles.Style16,
+                              )
+                            ],
+                          ),
+                        ),
+                        LineSetting(),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(Icons.check_circle_outline_rounded),
+                              SizedBox(
+                                width: 7.w,
+                              ),
+                              Text(
+                                'Terms of Use',
+                                style: Styles.Style16,
+                              )
+                            ],
+                          ),
+                        ),
+                      ]),
+                    ))
+              ],
+            ),
+          ),
+        ));
+  }
+}
+  
+*/
--- lib/screens/widgets/custom_settingrow.dart
+++ lib/screens/widgets/custom_settingrow.dart
@@ -1,15 +1,18 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_typing_uninitialized_variables, non_constant_identifier_names
 
 //import 'package:care_app_two/helper/constant.dart';
 import 'package:care_app_two/helper/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class SetRow extends StatelessWidget {
-  const SetRow({super.key, this.icon, this.text, this.color});
+  SetRow({super.key, this.icon, this.text});
 
   final text;
   final icon;
-  final color;
+  // final color;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -21,10 +24,41 @@ class SetRow extends StatelessWidget {
         ),
         Icon(
           icon,
+          color: Color(0xff0075FE),
           size: 55,
-          
-        ),
+        )
+
+        /* LiteRollingSwitch(
+          onSwipe: (){},
+          onDoubleTap: (){},
+          onTap: (){},
+          width: 34.h,
+         // height: 22.h,
+          animationDuration: Duration(milliseconds: 400),
+         /// borderRadius: BorderRadius.circular(24.0),
+          value: true,
+          colorOn: Color(0xff0075FE),
+          colorOff: Color(0xffB8B8B8),
+          iconOn: Icons.toggle_on,
+          iconOff: Icons.toggle_off,
+          textSize: 16.0,
+          onChanged: (bool state) {
+            //Use it when it is toggled
+            print('turned ${(state) ? 'on' : 'off'}');
+          }, 
+        )*/
       ],
     );
   }
 }
+
+/*Widget CustomSwitch(bool val, Function onChangedMethod) {
+  return CupertinoSwitch(
+      trackColor: Color(0xffB8B8B8),
+      activeColor: Color(0xff0075FE),
+      value: val,
+      onChanged: (newvalue) {
+        onChangedMethod(newvalue);
+      });
+}
+*/
