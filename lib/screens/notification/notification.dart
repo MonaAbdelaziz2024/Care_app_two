69aff02c23ffda13953705bd37d6edf6fb849243
monamohamedabdelazizmohamed@gmail.com <monamohamedabdelazizmohamed@gmail.com>
2024-06-27 22:49:24 +0300

    .

--- lib/screens/notification/notification.dart
+++ lib/screens/notification/notification.dart
@@ -48,7 +48,7 @@ class NotificationPage extends StatelessWidget {
                   )
                 ],
               )
-           ,ReminderCard(),
+          //  ,ReminderCard(),
             ],
           ),
         ));
